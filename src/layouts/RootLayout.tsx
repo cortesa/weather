7d7b1041e9fb41f5455394cbab3c57c5b548ec10
Andrés Cortés <cortesa@gmail.com>
2025-02-27 12:42:03 +0100

    Hidding dev tools

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
@@ -8,22 +8,9 @@ const queryClient = new QueryClient()
 export function RootLayout() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			{/* <div className="p-2 flex gap-2 text-blue-900">
-				<Link to="/" className="[&.active]:text-red-500">
-						Home
-				</Link>{" "}
-				<a 
-					href="https://about.cortesa.net" 
-					className="[&.active]:text-green-500" 
-					target="_blank" 
-					rel="noopener noreferrer" >
-						About
-				</a>
-			</div>
-			<hr /> */}
 			<Outlet />
-			<TanStackRouterDevtools />
-			<ReactQueryDevtools initialIsOpen={false} />
+			{/* <TanStackRouterDevtools /> */}
+			{/* <ReactQueryDevtools initialIsOpen={false} /> */}
 		</QueryClientProvider>
 	)
 }

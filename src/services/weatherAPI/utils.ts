558266111cb58e3140104d6c1b586bf797b86206
Andrés Cortés <cortesa@gmail.com>
2025-02-27 11:21:08 +0100

    fixed lint error

--- src/services/weatherAPI/utils.ts
+++ src/services/weatherAPI/utils.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from "axios"
 
-export const parseAxiosAPIError = (err: any) => {
+export const parseAxiosAPIError = (err: Error) => {
 	if (!err) return new Error("An unknown error occurred")
 	const parsedError = (err as AxiosError<{ error: string }>)?.response?.data?.error
 	return parsedError ? new Error(parsedError) : err
